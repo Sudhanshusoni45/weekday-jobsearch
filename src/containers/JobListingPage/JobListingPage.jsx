a45bdde9022403813e5d99c3b9377a4ba9fe05d8
sudhanshu <sudhanshusoni45@gmail.com>
2024-05-04 15:52:22 +0530

    feat: setup redux toolkit

--- src/containers/JobListingPage/JobListingPage.jsx
+++ src/containers/JobListingPage/JobListingPage.jsx
@@ -1,6 +1,12 @@
+import JobCard from "@components/JobListingPage/JobCard/JobCard";
 import styles from "./JobListingPage.module.css";
+
 const JobListingPage = () => {
-  return <div className={styles.Wrapper}></div>;
+  return (
+    <div className={styles.Wrapper}>
+      <JobCard />
+    </div>
+  );
 };
 
 export default JobListingPage;
