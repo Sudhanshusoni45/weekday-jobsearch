1e402bc85421642ec8b8e1209302fabe2aa51e37
sudhanshu <sudhanshusoni45@gmail.com>
2024-05-05 00:10:08 +0530

    feat: add loader

---
+++ src/components/LoaderWrapper/LoaderWrapper.jsx
@@ -0,0 +1,27 @@
+import styles from "./LoaderWrapper.module.css";
+
+const Loader = ({ loader, isDeleteButton, className }) => {
+  return (
+    <div
+      className={
+        className
+          ? `${className} ${styles.LoaderButton}`
+          : loader === "main"
+          ? styles.LoaderMain
+          : isDeleteButton
+          ? `${styles.LoaderButton} ${styles.LoaderRedButton}`
+          : styles.LoaderButton
+      }
+    ></div>
+  );
+};
+
+const LoaderWrapper = () => {
+  return (
+    <div className={styles.LoaderWrapper}>
+      <Loader />
+    </div>
+  );
+};
+
+export default LoaderWrapper;
--- src/containers/JobListingPage/JobListingPage.jsx
+++ src/containers/JobListingPage/JobListingPage.jsx
@@ -3,6 +3,7 @@ import styles from "./JobListingPage.module.css";
 import { useEffect } from "react";
 import useJobs from "@hooks/useJobs";
 import { useSelector } from "react-redux";
+import LoaderWrapper from "@components/LoaderWrapper";
 
 const JobListingPage = () => {
   const { handleGetJobs } = useJobs();
@@ -12,15 +13,16 @@ const JobListingPage = () => {
     handleGetJobs();
   }, []);
 
-  return (
+  // if the the network request is not completed show loading
+
+  return loading ? (
+    <LoaderWrapper />
+  ) : (
     <div className={styles.Wrapper}>
       {jobsData?.jdList?.length ? (
         jobsData?.jdList.map(({ jdUid, ...props }) => (
           <JobCard key={jdUid} {...props} />
         ))
-      ) : // if the the network request is not completed show loading
-      loading ? (
-        <h2>Loading...</h2>
       ) : (
         <div>No jobs found...</div>
       )}
