6277fe24022f18bb2bcb14e4acb8b42aaf2a0f2c
sudhanshu <sudhanshusoni45@gmail.com>
2024-05-04 23:39:00 +0530

    feat: style job card

--- src/components/JobListingPage/JobCard/JobCard.jsx
+++ src/components/JobListingPage/JobCard/JobCard.jsx
@@ -10,27 +10,65 @@ const JobCard = ({
 }) => {
   return (
     <div className={styles.Wrapper}>
-      <div>Posted 10 days ago</div>
-      <div>
-        <img src={logoUrl} alt={companyName} />
+      <div className={styles.PostedDateWrapper}>
+        <span className={styles.PostedText}>⏳ Posted a month ago</span>
+      </div>
+      <div className={styles.CompanyLogoWrapper}>
+        <img className={styles.CompanyLogo} src={logoUrl} alt={companyName} />
         <div className={styles.CompanyInfoWrapper}>
-          <span>{companyName}</span>
+          <span className={styles.CompanyName}>{companyName}</span>
           <span>{jobRole}</span>
           <span>{location}</span>
         </div>
       </div>
       <span>Estimated Salary: </span>
-      <div>
-        <span>About Company</span>
-        <span>About us</span>
-        <p>{jobDetailsFromCompany}</p>
+      <div className={styles.AboutWrapper}>
+        <span className={styles.AboutCompany}>About Company:</span>
+        <span className={styles.AboutUs}>About us</span>
+        <div className={styles.JobDetailsFromCompany}>
+          {jobDetailsFromCompany}
+        </div>
       </div>
-      <div>
-        <span>Minimun Experience </span>
+      <div className={styles.ViewJobWrapper}>
+        <span className={styles.ViewJob}> View Job</span>
+      </div>
+      <div className={styles.ExperienceWrapper}>
+        <span className={styles.MinExperience}>Minimun Experience </span>
         <span>2 years</span>
       </div>
-      <div>
-        <Button variant="contained">⚡ Easy Apply</Button>
+      <div className={styles.BtnWrapper}>
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: "var(--primary-green)",
+            padding: ".8rem 1.8rem",
+            fontSize: "1.6rem",
+            fontWeight: "500",
+            textTransform: "none",
+            color: "var(--pure-black)",
+            "&:hover": {
+              backgroundColor: "var(--primary-green)", // same as normal state
+              color: "var(--pure-black)", // same as normal state
+            },
+          }}
+        >
+          ⚡ Easy Apply
+        </Button>
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: "var(--primary-violet)",
+            padding: ".8rem 1.8rem",
+            fontSize: "1.6rem",
+            fontWeight: "500",
+            textTransform: "none",
+            "&:hover": {
+              backgroundColor: "var(--primary-violet)", // same as normal state
+            },
+          }}
+        >
+          Unlock referral asks
+        </Button>
       </div>
     </div>
   );
