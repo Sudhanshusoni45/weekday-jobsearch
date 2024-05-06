323eb4946de22950fd64e8c85c15a72f5924740f
sudhanshu <sudhanshusoni45@gmail.com>
2024-05-06 10:33:47 +0530

    feat:implement lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import JobListingPage from "@containers/JobListingPage/JobListingPage";
+import React, { Suspense } from "react";
 import "./App.css";
+import LoaderWrapper from "@components/LoaderWrapper";
+// lazy loading is not really needed for small applications, this is a demonstration of how it can be done
+const JobListingPage = React.lazy(
+  () => import("@containers/JobListingPage/JobListingPage")
+);
 
 function App() {
   return (
-    <>
+    <Suspense
+      fallback={
+        <div>
+          <LoaderWrapper />
+        </div>
+      }
+    >
       <JobListingPage />
-    </>
+    </Suspense>
   );
 }
 
